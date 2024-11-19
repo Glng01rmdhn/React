0df91d22e4bb0b8efc71b2494051fefa2545563e
glng01rmdhn <galang2909@student.abudzar.sch.id>
2024-11-19 12:19:09 +0800

    DUARRR!!!!!

--- react-apps/src/m2/App.jsx
+++ react-apps/src/m2/App.jsx
@@ -1,9 +1,35 @@
+// cd buat kalo misalnya npmnya error
+// mengambil data dari object dalam jsx
+const user = {
+  name: 'galang rambu anarki',
+  age: 30,
+country: 'indonesia',
+};
 function Testing(){
   return(
     <>
-    <h1>testing</h1>
+    <h1>PUSINGGGGGG!!!!!!!</h1>
+    <p>react ngajak berantrem</p>
+    <div>
+      <button style={{ backgroundColor: 'green', color: 'white' }}>Klik Saya</button>
+    </div>
+    {/* css dalam jsx */}
+    <h1 style={{ color: 'purple', stroke: 'red', fontSize: '90px' }}>jsx pake css gini kah?</h1>  
+
+    {/* mengambil data dari object */}
+    <p>nama : {user.name}</p>
+    <p>umur : {user.age}</p>
+    <p>negara : {user.country}</p>
+
+    {/* mengambil image */}
+
+    <img src="https://berita.99.co/wp-content/uploads/2022/04/puisi-ayah.jpg" alt="MY image" />
+
     </>
   )
 }
 
+
+
 export default Testing;
+
